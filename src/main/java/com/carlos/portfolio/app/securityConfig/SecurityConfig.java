f86578867cab2b84a9d744487964fa91833c10e8
Carlos-123321 <carlosfreestyle12@gmail.com>
2025-02-15 03:40:28 -0500

    Vercel fix

--- src/main/java/com/carlos/portfolio/app/securityConfig/SecurityConfig.java
+++ src/main/java/com/carlos/portfolio/app/securityConfig/SecurityConfig.java
@@ -1,20 +1,17 @@
 package com.carlos.portfolio.app.securityConfig;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // Allows all origins, methods, and headers
         registry.addMapping("/**")
-                .allowedOrigins("*") // Allow all origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Allow all HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true) // Allow credentials if needed
-                .maxAge(3600); // Cache CORS pre-flight requests for 1 hour
+                .allowedOrigins("https://carlos-portfolio-orcin.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
